ffc562e78c55594d9dcf10faa7e16a6e51ab054b
yoon052 <ynj3126@hanmail.net>
2022-08-06 19:23:24 +0900

    second commit

---
+++ src/components/Router.js
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import Auth from "../routes/Auth";
+import Home from "../routes/Home";
+
+const AppRouter = () => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false); //eslint-disable-line no-unused-vars
+    return (
+        <Router>
+            <Routes>    //v6 버전으로 인해 Switch가 Routes로 변경됨 //component가 element로 바뀜
+                {isLoggedIn ? (
+                    <Route path="/" element={<Home />}>    //exact 안쓴다                        
+                    </Route>
+                ) : (
+                    <Route path="/" element={<Auth />}>    //exact 안쓴다                        
+                    </Route>
+                )}
+            </Routes>
+        </Router>
+    )
+}
+
+export default AppRouter;
